10bf662b73fb750671efac98f6eecf033dee8861
Akash Rajendra Kanase <akashkanase2005@gmail.com>
2026-01-20 00:34:17 +0530

    Update ddl_gold_layer.sql

--- scripts/gold_layer/ddl_gold_layer.sql
+++ scripts/gold_layer/ddl_gold_layer.sql
@@ -4,7 +4,7 @@ GO
 
 CREATE VIEW gold.dim_customers AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, 
     ci.cst_id                          AS customer_id,
     ci.cst_key                         AS customer_number,
     ci.cst_firstname                   AS first_name,
@@ -12,8 +12,8 @@ SELECT
     la.cntry                           AS country,
     ci.cst_marital_status              AS marital_status,
     CASE 
-        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
-        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr 
+        ELSE COALESCE(ca.gen, 'n/a')  			   
     END                                AS gender,
     ca.bdate                           AS birthdate,
     ci.cst_create_date                 AS create_date
